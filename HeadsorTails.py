642cc1e50fccb94d4a14e084dca03437c974d330
YAKUSHINJI Taisei <y.taisei@YAKUSHINJInoMacBook-Air.local>
2022-07-10 20:52:16 +0900

    HoT: initial revision

---
+++ HeadsorTails.py
@@ -0,0 +1,20 @@
+import random
+
+heads = 0
+tails = 0
+total = 0
+print("Tossing a coin...")
+
+while total < 3:
+    total += 1
+    randomInt = random.randint(0, 1)
+
+    if randomInt == 1:
+        print("Round %d: Heads" % total)
+        heads += 1
+    else:
+        print("Round %d: Tails" % total)
+        tails += 1
+
+print("Heads: %d, Tails: %d" % (heads,tails))
+
